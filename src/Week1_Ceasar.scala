a1c450975ff8edc30a4a2bf56fc2f2b556f4d496
Joachim <Joachim.e.nebel@gmail.com>
2023-01-29 22:49:27 +0100

    Week 1

---
+++ src/Week1_Ceasar.scala
@@ -0,0 +1,33 @@
+object Week1_Ceasar extends App {
+
+  /*
+    Cæsar cipher er en simpel krypteringsmetode der virker ved
+    at forskyde bogstaver med et bestemt offset i alfabetet.
+    Implementér en funktion der kan deciphrere en krypteret tekststreng.
+    Cifre forskydes på samme måde. Alle andre tegn forskydes ikke.
+    Ved overflow startes der forfra ved 'A' for bogstaver og ved '0' for cifre.
+    Se vejledende tests for eksempler
+  */
+
+  // Implenetér denne funktion
+  def decipher(cipherText: String, offset: Int): String = {
+    ???
+  }
+
+
+  // Vejledende tests
+  assert(decipher("c", 2) == "a")
+  assert(decipher("C", 2) == "A")
+  assert(decipher("6", 2) == "4")
+  assert(decipher(".", 17) == ".")
+
+  assert(decipher("6", 10) == "6")
+  assert(decipher("A", 26) == "A")
+  assert(decipher("N", 10) == "D")
+
+  assert(decipher("Pmf 9 Nmzsqdl!", 12) == "Dat 7 Bangerz!")
+  assert(decipher("Wxkotkxgztgzxgtgwwxkaxwhex. wxk otk ytdmbld 78", 123) == "Dervarengangenandderhedole. der var faktisk 45")
+  assert(decipher("UhmnahKhzkm<.hzhz()hzk", 7) == "NafgtaDasdf<.asas()asd")
+
+  println("All tests passed succesfully!")
+}
