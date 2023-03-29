e51dba20eac5280299429eda161a0bec9d33addf
Joachim <Joachim.e.nebel@gmail.com>
2023-03-29 13:44:02 +0200

    Sæsonafslutning

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ Leaderboard opdateres ved hver holdets time.\
 Point virker ligesom sidste semester. Dvs. hvis _n_ har sendt en løsning, får 1. pladsen _n_ point, 2. pladsen får _n-1_ osv.
 
 Efter ugen er afgjort indsættes ugens bedste løsning her på GitHub.
+
+Lasse er vinderen af sæsonen wup wup tillykke!
--- src/Week8_RockPaperScissor.scala
+++ src/Week8_RockPaperScissor.scala
@@ -1,9 +1,19 @@
-object Week8_RockPaperScissor {
+object Week8_RockPaperScissor extends App {
   /*
     Gæt sammenhængen og lav en løsning
   */
   def winnerWinner(p1: String, p2: String): String = {
-    ???
+    /*
+      Daniels løsning der ikke compippler:
+      print("hi, mom!")
+    */
+
+    // Lasses løsning på 334 chars
+    val l=Seq("Sten","Saks","Papir","Rock","Paper","Scissor","Uafgjort")
+    val o=l.indexOf(p1)
+    val s=l.indexOf(p2)
+    val m=Map((0,0)->6,(0,1)->0,(0,2)->2,(0,3)->6,(0,4)->4,(0,5)->0,(1,1)->6,(1,2)->1,(1,3)->3,(1,4)->1,(1,5)->6,(2,2)->6,(2,3)->2,(2,4)->6,(2,5)->5,(3,3)->6,(3,4)->4,(3,5)->3,(4,4)->6,(4,5)->5,(5,5)->6)
+    l(m((o.min(s),s.max(o))))
   }
 
   assert(winnerWinner("Sten", "Saks") == "Sten")
